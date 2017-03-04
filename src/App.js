a17dfeefef6d2c8eeeac0745ad1d13f039d6d743
grraft <kunal.mandalia@grraft.com>
2017-03-04 03:06:42 +0000

    Add CodePen content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const Heading = <h1 className='lead text-primary lead-heading'>On the Shoulders of Giants</h1>;
+const Subheading = <h2 className='subheading'>Folks pushing the web forward</h2>;
+
+const Giant = ({giant}) => {
+  return (
+      <div className='row'>
+      <div className='col-xs-12 col-md-6 col-sm-offset-0 col-md-offset-3'>
+        <div className='giant'>
+        <img src={giant.img_url} className='img-responsive' />
+        <h3>
+          {giant.people.map((p,i, people) => { return <span key={i}>{p}{i<people.length-1 ? ', ' : ''}</span>})}</h3>
+        <hr />
+          <h4><a href={giant.link}>{giant.knownFor}</a></h4>
+        <hr />
+        <h5>{giant.organisation}</h5>
       </div>
-    );
-  }
+      </div>
+    </div>
+  );
 }
 
-export default App;
+const TributePage = ({giants}) => {
+  return (
+    <div className='container text-center page'>
+      {Heading}
+      {Subheading}
+      {giants.map((g, i, giants) => {
+        return <Giant giant={g} />
+      })}
+    </div>
+  )
+}
+
+export default TributePage;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,38 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const data = [
+  {
+    people: ['Jordan Walke', 'Dan Abramov'],
+    organisation: 'Facebook',
+    knownFor: 'React Redux',
+    img_url: 'https://www.filepicker.io/api/file/xwCZZhShQLS247e9uC4D',
+    link: 'https://facebook.github.io/react/',
+  },
+  {
+    people: ['Quincy Larson'],
+    organisation: 'FreeCodeCamp',
+    knownFor: 'Learning',
+    img_url: 'https://pbs.twimg.com/profile_images/378800000147359764/54dc9a5c34e912f34db8662d53d16a39_400x400.png',
+    link: 'https://www.freecodecamp.com',
+  },
+  {
+    people: ['Paul Irish'],
+    organisation: 'Google',
+    knownFor: 'Chrome DevTools',
+    img_url: 'https://pbs.twimg.com/profile_images/809064074998710272/KJvmreRz.jpg',
+    link: 'https://developer.chrome.com/devtools',
+  },
+  {
+    people: ['TJ Hollowaychuk'],
+    organisation: 'Google',
+    knownFor: 'Express.js',
+    img_url: 'https://pbs.twimg.com/profile_images/378800000226613002/36623ae09f553713c575c97c77544b49.jpeg',
+    link: 'https://github.com/tj',
+  },
+];
+
 ReactDOM.render(
-  <App />,
+  <App giants={data} />,
   document.getElementById('root')
 );
